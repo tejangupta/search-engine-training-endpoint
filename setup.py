5139002a10e43eab86b5195a5150acf206aa2726
Tejan Gupta <tejangupta8@gmail.com>
2023-09-14 14:10:21 +0000

    requirements and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tejan Gupta",
+    author_email="tejangupta8@gmail.com",
+    packages=find_packages()
+)
